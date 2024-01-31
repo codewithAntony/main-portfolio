8e2cdbdae7d19adae8420ae870943930dae2d236
Antony Murithi <antonymurithi51@gmail.com>
2024-01-31 13:09:29 +0300

    My portfolio

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-l'>
+            <h1>Antony Namu</h1>
+            <ul className='header-nav'>
+                <li><a href="#">Past Work</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Testimonials</a></li>
+            </ul>
+            </div>
+
+            <div className='header-r'>
+                <div className='hire'>
+                    <a href='#'>Hire Me -></a>
+                </div>
+                
+            </div>
+
+        </div>
+        
+    )
+}
+
+export default Header
