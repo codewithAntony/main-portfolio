1bb7da252b28e4f22312eba94c9bb0b2d13ef01a
Antony Murithi <antonymurithi51@gmail.com>
2024-06-09 19:46:58 +0300

    update header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom'
+import React, { useRef } from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    const handleClick = () => {
-        window.location.href = 'mailto:${antonymurithi51@gmail.com'
-    }
-    
-    return (
-        <div className='header'>
-            <div className='header-l'>
-            <h1>Antony Namu</h1>
-            <ul className='header-nav'>
-                <li><a href="#">Past Work</a></li>
-                <li><a href="skills">Skill</a></li>
-                <li><a href="#">Testimonial</a></li>
-            </ul>
-        </div>
+    const skillsRef = useRef(null);
 
-            <div className='header-r'>
-                <div className='hire'>
-                    <a href={'mailto:${antonymurithi51@gmail.com}'}
-                    onClick={handleClick}>Hire Me -></a>
+    const scrollToSection = (sectionRef) => {
+        sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    const handleClick = () => {
+        window.location.href = 'mailto:${antonymurithi51@gmail.com';
+    };
+
+    return (
+        <div className="header">
+            <div className="header-l">
+                <h1>Antony Namu</h1>
+                <ul className="header-nav">
+                    <li>
+                        <a href="#">Past Work</a>
+                    </li>
+                    <li onClick={() => scrollToSection(skillsRef)}>Skills</li>
+                    <li>
+                        <a href="#">Testimonial</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="header-r">
+                <div className="hire">
+                    <a
+                        href={'mailto:${antonymurithi51@gmail.com}'}
+                        onClick={handleClick}
+                    >
+                        Hire Me ->
+                    </a>
                 </div>
             </div>
         </div>
-        
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
--- src/Components/Skill/Skill.jsx
+++ src/Components/Skill/Skill.jsx
@@ -1,45 +1,47 @@
-import React from 'react'
-import './Skill.css'
-import Tick from '../../assets/tick-2.png'
+import React from 'react';
+import './Skill.css';
+import Tick from '../../assets/tick-2.png';
 function Skill() {
     return (
-        <div className='skills'>
+        <div className="skills">
             <div className="skills-t">
                 <h2>Skills & Technologies</h2>
-                <p>I am skilled Web Developer with over 5 Years of freelance experience. <br /> A self-motivated and self-driven individual who is always looking for new opportunities to grow and improve.</p>
+                <p>
+                    I am a skilled Web Developer with over 5 Years of freelance
+                    experience. <br /> A self-motivated and self-driven
+                    individual who is always looking for new opportunities to
+                    grow and improve.
+                </p>
                 <p>My skillsets: </p>
             </div>
             <div className="skills-b">
-                <div className='skillsets'>
+                <div className="skillsets">
                     <img src={Tick} alt="" />
                     <p>Javascript</p>
                 </div>
-                <div className='skillsets'>
+                <div className="skillsets">
                     <img src={Tick} alt="" />
                     <p>Python</p>
                 </div>
-                <div className='skillsets'>
+                <div className="skillsets">
                     <img src={Tick} alt="" />
                     <p>CSS</p>
                 </div>
-                <div className='skillsets'>
-                    <img src={Tick}alt="" />
+                <div className="skillsets">
+                    <img src={Tick} alt="" />
                     <p>React</p>
                 </div>
-                <div className='skillsets'>
+                <div className="skillsets">
                     <img src={Tick} alt="" />
                     <p>Typescript</p>
                 </div>
-                <div className='skillsets'>
+                <div className="skillsets">
                     <img src={Tick} alt="" />
                     <p>Django</p>
                 </div>
             </div>
-            
-
-        
         </div>
-    )
+    );
 }
 
-export default Skill
+export default Skill;
